e3180f4e46e996b4758a38cd2ec2b311140e80c2
jsukrut <surkut.j@indictranstech.com>
2018-10-31 12:47:46 +0530

    [FIX][Be 0270][allow more than 50 comments]

--- frappe/core/doctype/communication/comment.py
+++ frappe/core/doctype/communication/comment.py
@@ -21,7 +21,7 @@ def validate_comment(doc):
 			and reference_name=%(reference_name)s""",
 		{"reference_doctype": doc.reference_doctype, "reference_name": doc.reference_name})[0][0]
 
-	if comment_count >= 50:
+	if comment_count >= 100:
 		frappe.throw(_("Cannot add more than 50 comments"))
 
 def on_trash(doc):
